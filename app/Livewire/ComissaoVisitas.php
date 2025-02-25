6427ffab2e044c2c792d09feda4405084af1575b
Waleriano Magalhaes <waleriano.magalhaes@farmaciaindiana.com.br>
2025-02-25 20:17:48 -0300

    Refactor: corrigindo funcao que busca comissao

--- app/Livewire/ComissaoVisitas.php
+++ app/Livewire/ComissaoVisitas.php
@@ -22,10 +22,10 @@ class ComissaoVisitas extends Component
                     ->join('objetivos as o', 'v.id_objective', '=', 'o.id')
                     ->select(
                         'p.name as promotor',
-                        DB::raw('COUNT(CASE WHEN o.id = 1 THEN 1 END) AS captacao'),
-                        DB::raw('COUNT(CASE WHEN o.id = 2 THEN 1 END) AS loja'),
-                        DB::raw('COUNT(CASE WHEN o.id = 3 THEN 1 END) AS manutencao'),
-                        DB::raw('(COUNT(v.id) * 2) AS total_a_pagar')
+                        DB::raw('COUNT(CASE WHEN visitas_convenio_o.id = 1 THEN 1 END) AS captacao'),
+                        DB::raw('COUNT(CASE WHEN visitas_convenio_o.id = 2 THEN 1 END) AS loja'),
+                        DB::raw('COUNT(CASE WHEN visitas_convenio_o.id = 3 THEN 1 END) AS manutencao'),
+                        DB::raw('(COUNT(visitas_convenio_v.id) * 2) AS total_a_pagar')
                     )
                     ->whereBetween('v.date', [$this->dataIni, $this->dataFim])
                     ->groupBy('p.id', 'p.name')
