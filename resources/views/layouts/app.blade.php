5816cfdf3f1e4d19f298c84ff1126832f678e576
Waleriano Magalhaes <waleriano.magalhaes@farmaciaindiana.com.br>
2025-03-01 15:07:09 -0300

    Feat: Atulizando icone do site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>Visitas Convênio</title>
 
+        <link rel="shortcut icon" type="imagex/png" size="16x16" href="assets/images/logo.ico">
+        
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
